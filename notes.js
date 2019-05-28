9de81e7c9e2ac3c920061d7e8874fc3a4ccf0e2e
Steve Mckinnon <elevated42htc@gmail.com>
2019-05-28 19:56:28 +1000

    backgrounds

---
+++ notes.js
@@ -0,0 +1,29 @@
+.container {
+	display: flex;
+	flex-wrap: wrap;
+}
+
+.container > div {
+	background-color: springgreen;
+	font-size: 20px;
+	margin: 20px;
+	padding: 20px;
+	width: 20px;
+}
+
+
+
+cssfile.cscc
+
+$variable_name: proprty;
+
+ul {
+	font-size: 14px;
+	color: $variable_name;
+}
+
+ol {
+	font-size: 18px;
+	color: variable_name;
+	
+}
